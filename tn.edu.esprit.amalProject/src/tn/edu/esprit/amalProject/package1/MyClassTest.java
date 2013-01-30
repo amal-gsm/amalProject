622605ae8b41f6eaa8826c01d99e8441835257d5
Amoula <Amoula@Amoula-PC>
2013-01-30 10:31:35 +0100

    Signed-off-by: Amoula <Amoula@Amoula-PC>

---
+++ tn.edu.esprit.amalProject/src/tn/edu/esprit/amalProject/package1/MyClassTest.java
@@ -0,0 +1,13 @@
+package tn.edu.esprit.amalProject.package1;
+
+public class MyClassTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
